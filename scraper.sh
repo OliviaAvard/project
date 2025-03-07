c7d0b1093133e9e392b216c848b199b554fec777
OliviaAvard <oavard@gmail.com>
2025-03-07 22:26:56 +0100

    new data

--- scraper.sh
+++ scraper.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
 # Define URL
-URL="https://fr.finance.yahoo.com/quote/MC.PA/"
+URL="https://www.coingecko.com/en/coins/pi-network"
 
 # Fetch HTML content
 HTML=$(curl -s "$URL")
 
-# Extract LVMH stock price using regex
-#PRICE=$(echo "$HTML" | grep -oP '(?<="regularMarketPrice":{"raw":)[0-9]+\.[0-9]+')
-PRICE=$(echo "$HTML" | grep -oP '"currentPrice":\{"raw":\K[0-9]+\.[0-9]+' )
-
+# Extract Pi Network price using regex
+PRICE=$(echo "$HTML" | grep -oP '(?<=<span class="no-wrap">\$)[0-9,]+(\.[0-9]+)?')
 
 # Get current timestamp
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 
 # Save to CSV file
-echo "$TIMESTAMP, $PRICE" >> lvmh_prices.csv
+echo "$TIMESTAMP, $PRICE" >> pi_network_prices.csv
 
