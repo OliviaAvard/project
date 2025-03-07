be85e8598413f62b552afbbcac6c67e186d0f8aa
OliviaAvard <oavard@gmail.com>
2025-03-07 22:21:33 +0100

    scraper modified and csv

--- scraper.sh
+++ scraper.sh
@@ -7,7 +7,9 @@ URL="https://fr.finance.yahoo.com/quote/MC.PA/"
 HTML=$(curl -s "$URL")
 
 # Extract LVMH stock price using regex
-PRICE=$(echo "$HTML" | grep -oP '(?<="regularMarketPrice":{"raw":)[0-9]+\.[0-9]+')
+#PRICE=$(echo "$HTML" | grep -oP '(?<="regularMarketPrice":{"raw":)[0-9]+\.[0-9]+')
+PRICE=$(echo "$HTML" | grep -oP '"currentPrice":\{"raw":\K[0-9]+\.[0-9]+' )
+
 
 # Get current timestamp
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
