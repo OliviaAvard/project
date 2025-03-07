bcdc79a1a1a183493cdf103639a4018a4e17319f
OliviaAvard <oavard@gmail.com>
2025-03-07 22:29:40 +0100

    new data 2

--- scraper.sh
+++ scraper.sh
@@ -7,7 +7,8 @@ URL="https://www.coingecko.com/en/coins/pi-network"
 HTML=$(curl -s "$URL")
 
 # Extract Pi Network price using regex
-PRICE=$(echo "$HTML" | grep -oP '(?<=<span class="no-wrap">\$)[0-9,]+(\.[0-9]+)?')
+#PRICE=$(echo "$HTML" | grep -oP '(?<=<span class="no-wrap">\$)[0-9,]+(\.[0-9]+)?')
+PRICE=$(echo "$HTML" | grep -oP '(?<=<div class="coin-value">.*?<span class="no-wrap">\$)[0-9,]+\.[0-9]+')
 
 # Get current timestamp
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
